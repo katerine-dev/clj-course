516aea57ae32d4fcc20007ad32d3faddc6a27034
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-01-24 13:41:41 -0300

    first class

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to alura-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/alura_clj/core.clj
@@ -0,0 +1,48 @@
+(ns alura-clj.core
+  (:require [clojure.string :as str]))
+
+; Define simbolo
+(println "Bem vindo")
+(def total-de-produtos 15)
+
+(println total-de-produtos)
+(println "Total" total-de-produtos)
+(+ 13 3)
+(> 1 3)
+(< 1 3)
+
+(or (< 1 3) (> 5 6))
+
+; vetor
+(def estoque ["Mochila" "Camisa"])
+; como acessa o primeiro elemento
+(estoque 0)
+
+(["pablo" "kate" "boris"] 2)
+
+(str/join " " ["eu" "comi" "lasanha"])
+; incluir mais um elemento no estoque - obs (imutável)
+(conj estoque "Cadeira")
+
+(def estoque (conj estoque "Cadeira "))
+
+; função e parâmetros
+
+(defn imprime-mensagem
+  []
+  (println "------------")
+  (println "Bem vindo(a) ao estoque!!"))
+
+(imprime-mensagem)
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% no valor bruto"
+  [valor-bruto]
+  (* valor-bruto (- 1 0.1)))
+
+(valor-descontado 50)
+
+; Verbo é efeito colateral ("realizar", ir fora do programa)
+; Substantivo código puro (me devolve esse valor)
+
+
